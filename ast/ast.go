0e5e016237354baead39de39cbc8167220bce1d2
tkinaba <tiago.inaba@gmail.com>
2024-07-02 22:33:12 -0300

    2.4 done

--- ast/ast.go
+++ ast/ast.go
@@ -41,3 +41,6 @@ type Identifier struct {
 	Token token.Token
 	Value string
 }
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
